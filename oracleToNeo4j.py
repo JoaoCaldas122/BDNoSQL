495eb3ea126bad21e46a5131f1fab05700e22abd
Thatch186 <andrepizarromartins@gmail.com>
2023-06-01 16:34:35 +0100

    Neo4j ligado

--- oracleToNeo4j.py
+++ oracleToNeo4j.py
@@ -2,7 +2,7 @@ import cx_Oracle
 from py2neo import Graph
 
 # Connect to Oracle
-oracle_conn = cx_Oracle.connect(user="uminho", password="uminho2020", dsn="localhost:1521/xe")
+oracle_conn = cx_Oracle.connect(user="store", password="pass", dsn="localhost:1521/xe")
 oracle_cursor = oracle_conn.cursor()
 
 # Connect to Neo4j
@@ -13,7 +13,7 @@ oracle_cursor.execute('SELECT * FROM store_users')
 users_data = oracle_cursor.fetchall()
 
 for user in users_data:
-    user_id = user['user_id']
+    user_id = user[0]
     
     # Create user node
     graph.run(
@@ -31,14 +31,14 @@ for user in users_data:
         }
         ''',
         user_id=user_id,
-        first_name=user['first_name'],
-        middle_name=user['middle_name'],
-        last_name=user['last_name'],
-        phone_number=user['phone_number'],
-        email=user['email'],
-        username=user['username'],
-        user_password=user['user_password'],
-        registered_at=user['registered_at']
+        first_name=user[1],
+        middle_name=user[2],
+        last_name=user[3],
+        phone_number=user[4],
+        email=user[5],
+        username=user[6],
+        user_password=user[7],
+        registered_at=user[8]
     )
 
     # Create shopping session relationships
@@ -46,7 +46,7 @@ for user in users_data:
     sessions_data = oracle_cursor.fetchall()
 
     for session in sessions_data:
-        session_id = session['session_id']
+        session_id = session[0]
 
         # Create session node
         graph.run(
@@ -58,8 +58,8 @@ for user in users_data:
             }
             ''',
             session_id=session_id,
-            created_at=session['created_at'],
-            modified_at=session['modified_at']
+            created_at=session[1],
+            modified_at=session[2]
         )
 
         # Create relationship between user and session
@@ -77,7 +77,7 @@ for user in users_data:
         cart_items_data = oracle_cursor.fetchall()
 
         for cart_item in cart_items_data:
-            product_id = cart_item['product_id']
+            product_id = cart_item[2]
 
             # Create cart item node
             graph.run(
@@ -91,9 +91,9 @@ for user in users_data:
                 ''',
                 product_id=product_id,
                 session_id=session_id,
-                quantity=cart_item['quantity'],
-                created_at=cart_item['created_at'],
-                modified_at=cart_item['modified_at']
+                quantity=cart_item[3],
+                created_at=cart_item[4],
+                modified_at=cart_item[5]
             )
 
             # Create relationship between session and cart item
@@ -111,8 +111,8 @@ oracle_cursor.execute('SELECT * FROM product')
 products_data = oracle_cursor.fetchall()
 
 for product in products_data:
-    category_id = product['category_id']
-    discount_id = product['discount_id']
+    category_id = product[2]
+    discount_id = product[5]
 
     # Create product node
     graph.run(
@@ -126,12 +126,12 @@ for product in products_data:
             last_modified: $last_modified
         }
         ''',
-        product_id=product['product_id'],
-        product_name=product['product_name'],
-        sku=product['sku'],
-        price=product['price'],
-        created_at=product['created_at'],
-        last_modified=product['last_modified']
+        product_id=product[0],
+        product_name=product[1],
+        sku=product[3],
+        price=product[4],
+        created_at=product[6],
+        last_modified=product[7]
     )
 
     # Create relationship between product and category
@@ -143,7 +143,7 @@ for product in products_data:
         MATCH (p:Product {product_id: $product_id}), (c:Category {category_id: $category_id})
         MERGE (p)-[:BELONGS_TO_CATEGORY]->(c)
         ''',
-        product_id=product['product_id'],
+        product_id=product[0],
         category_id=category_id
     )
 
@@ -157,7 +157,7 @@ for product in products_data:
             MATCH (p:Product {product_id: $product_id}), (d:Discount {discount_id: $discount_id})
             MERGE (p)-[:HAS_DISCOUNT]->(d)
             ''',
-            product_id=product['product_id'],
+            product_id=product[0],
             discount_id=discount_id
         )
 
